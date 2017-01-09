05105d55deddeb6023946a7f4f22321ff214b61c
woodc9217 <woodc9217@HRH-IC0027333.wrdsb.ca>
2017-01-09 09:24:45 -0500

    final project

---
+++ src/finalproject/candice/FinalProjectCandice.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject.candice;
+
+/**
+ *
+ * @author woodc9217
+ */
+public class FinalProjectCandice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
